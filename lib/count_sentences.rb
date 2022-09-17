19a5546e43d6080963c00a27f9968d2195e51984
Charles <c_onyemem@live.com>
2022-09-17 13:32:51 -0700

    commit tree 528874c72dda1a6c6145e3ae7e55d01b4c419799

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
+   self[-1] == "." ?
+    
+      true
+      :
+      false
     
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+   starter_str = self.split(/[!?.]/)
+   new_str = starter_str.filter {|ele| ele != ""}
+   new_str.length
+  
   end
 end
+
